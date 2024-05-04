2b450f4a14c57a5e7934ab65fa7862eb27b8f8c2
Juan Pablo <juanpablolc414@gmail.com>
2024-05-04 14:06:06 -0500

    herencia y encapsulamiento geometrico

---
+++ src/FiguraGeometrica.java
@@ -0,0 +1,22 @@
+public class FiguraGeometrica {
+    private String Nombre;
+    private String Color;
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+
+}
---
+++ src/Rectangulo.java
@@ -0,0 +1,30 @@
+public class Rectangulo extends FiguraGeometrica implements Calculos{
+    private float alto;
+    private float largo;
+
+    public float getAlto() {
+        return alto;
+    }
+
+    public void setAlto(float alto) {
+        this.alto = alto;
+    }
+
+    public float getLargo() {
+        return largo;
+    }
+
+    public void setLargo(float largo) {
+        this.largo = largo;
+    }
+
+    @Override
+    public void perimetro() {
+        System.out.println("el perimetro es: " + 2*(getAlto() + getLargo()));
+    }
+
+    @Override
+    public void area() {
+        System.out.println("el area del rectangulo es: " + (getAlto() * getLargo()));
+    }
+}
